56ebd582f26b7cda5b497884842edc2b377ebb7e
Joey Freund <joeyfreund@gmail.com>
2015-06-10 23:41:58 -0400

    Basic first version, includes tags (with filtering).

--- js/front-docs.js
+++ js/front-docs.js
@@ -3,77 +3,64 @@
 var FrontDocs = angular.module('FrontDocs', ['ui.router']);
 
 
-FrontDocs.factory('DataService', function() {
-  var DataService = {};
 
-  DataService.getItems = function() { return [
-
-	{title: 'Pacer Docs', url: 'http://xnlogic.github.io/pacer', 
-  	 icons: ['help'],
-  	 tags: ['doc', 'pacer'],
-  	 thumbnail_url: 'img/thumbnails/pacer-docs.jpeg',
-  	 description: 'Pacer documentation website. Pacer is an open-source, JRuby library for efficient graph traversal and extensible data modeling.'
-  	},
-
-  	{title: 'XN Wiki', url: 'https://github.com/xnlogic/xnlogic/wiki', 
-  	 icons: ['help', 'construction'],
-  	 tags: ['doc', 'xn', 'wiki', 'in-progress'],
-  	 thumbnail_url: 'img/thumbnails/xn-wiki.jpeg',
-  	 description: 'Unofficial documentation of the XN framework. Feel free to add/edit the wiki.'
- 	},
-
-  	{title: 'Blueprints',  url: 'https://github.com/xnlogic/blueprints', 
-  	 icons: ['code'],
-  	 tags: ['code', 'blueprints', 'repo'],
-  	 thumbnail_url: 'img/thumbnails/blueprints-repo.jpeg',
-  	 description: 'Blueprints library (forked from Tinkerpop) - A set of unified interfaces for graph operations, implementation of adapters for a few graph DB\'s (e.g. Neo4j) as well as an in-memory graph called TinkerGraph.'
-  	},
-
-  	{title: 'Intro to XN',  url: 'http://slides.com/xnlogic/xn-intro', 
-  	 icons: ['presentation'],
-  	 tags: ['xn', 'slide', 'doc'],
-  	 thumbnail_url: 'img/thumbnails/intro-to-xn.jpg',
-  	 description: 'Training session slides'
-  	},
-
-	{title: 'XN - Getting Started Tutorial',  url: 'http://xnlogic.github.io/getting_started/', 
-  	 icons: ['tutorial'],
-  	 tags: ['xn', 'doc', 'tutorial'],
-  	 thumbnail_url: 'img/thumbnails/xn-getting-started.jpeg',
-  	 description: 'Includes code examples and walkthroughs'
-  	}
+FrontDocs.controller('FrontDocsHomeCtrl', function($scope, $http) {
+	var selectedTags = new Set();
 
 
-  	];
-  };
+	// Get the data from a JSON file ...
+	$http.get('data/front-docs.json')
+        .success(function (data) {
+            
+            $scope.pageHeader = data.pageHeader;
+            $scope.docItems   = data.frontDocsItems;
 
-  DataService.getPageHeader = function() { return 'XN Logic - FrontDocs' };
+			// Collect tags from all items ...
+			var tags = new Set();
+			$scope.docItems.forEach(function(docItem){
+				docItem.tags.forEach(function(tag){
+					tags.add(tag);
+				});
+			});
+
+			// Save the tags in an array (to allow the template to ng-repeat through them)
+			$scope.tags = [];
+			tags.forEach(function(tag){$scope.tags.push(tag);});
+			
+
+        })
+
+        .error(function (data, status, headers, config) {
+            //  TODO: Error handling here!
+        }
+    );
 
 
-  DataService.getTags = function() {
-  		var tags = new Set();
-  		var items = DataService.getItems();
-  		for (var i = 0; i < items.length; i++) {
-  			var item = items[i];
-  			if(item.tags){
-  				item.tags.forEach(function(tag){tags.add(tag);})
-  			}
-  		};
-  		return tags;
-  };
+    // Scope function to handle the different states of the tag buttons ...
 
+	$scope.toggleTag = function(tag){
+		if(selectedTags.has(tag)){
+			selectedTags.delete(tag);
+		} else {
+			selectedTags.add(tag);			
+		}
+	};
 
-  return DataService;
-});
+	$scope.tagIsOn = function(tag){
+		return selectedTags.has(tag);
+	};
 
+	$scope.shouldDisplayItem = function(frontDocsItem){
+		var result = true;
 
+		// FIXME: Short-circuit it
+		selectedTags.forEach(function(tag){
+			result = result && frontDocsItem.tags && frontDocsItem.tags.indexOf(tag) > -1;
+		});
 
+		return result;
+	};
 
-FrontDocs.controller('FrontDocsHomeCtrl', function($scope, DataService) {
-	$scope.pageHeader = DataService.getPageHeader();
-	$scope.docItems  = DataService.getItems();
-	$scope.tags = [];
-	DataService.getTags().forEach(function(tag){$scope.tags.push(tag);});
 });
 
 
